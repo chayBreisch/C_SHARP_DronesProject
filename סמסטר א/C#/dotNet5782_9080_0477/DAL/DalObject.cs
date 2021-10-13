103222b7c57449fa63a0b7bad8a8efc2fb3040da
chayBreisch <Handesaim3A5@mbjcomp.org.il>
2021-10-13 18:37:07 +0300

    add function to pull dataSource

---
+++ סמסטר א/C#/dotNet5782_9080_0477/DAL/DalObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+namespace DalObject
+{
+    class DalObject
+    {
+        public DalObject()
+        {
+            DataSource.Initialize();
+        }
+        public Drone[] GetDrone()
+        {
+            Drone[] Drones = new Drone[DataSource.Config.DronesIndex];
+            for (int i = 0; i < DataSource.Config.DronesIndex; i++)
+            {
+                Drones[i] = DataSource.Drones[i];
+            }
+            return Drones;
+        }
+        public Station[] GetStation()
+        {
+            Station[] Stations = new Station[DataSource.Config.StationIndex];
+
+            for (int i = 0; i < DataSource.Config.StationIndex; i++)
+            {
+                Stations[i] = DataSource.Stations[i];
+            }
+            return Stations;
+        }
+        public Customer[] GetCustomer()
+        {
+            Customer[] Customers = new Customer[DataSource.Config.CustomerIndex];
+            for (int i = 0; i < DataSource.Config.CustomerIndex; i++)
+            {
+                Customers[i] = DataSource.Customers[i];
+            }
+            return Customers;
+        }
+        public Parcial[] GetParcial()
+        {
+            Parcial[] Parcials = new Parcial[DataSource.Config.ParcialIndex];
+            for (int i = 0; i < DataSource.Config.CustomerIndex; i++)
+            {
+                Parcials[i] = DataSource.Parcials[i];
+            }
+            return Parcials;
+        }
+    }
+
+}
