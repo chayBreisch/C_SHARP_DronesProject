14a29bf51abe0fae39f46dce8064b9f0a7a514c6
chayBreisch <Handesaim3A5@mbjcomp.org.il>
2021-10-26 15:53:12 +0300

    change the arrays in dataSource to generic list

--- סמסטר א/C#/dotNet5782_9080_0477/DAL/DataSource.cs
+++ סמסטר א/C#/dotNet5782_9080_0477/DAL/DataSource.cs
@@ -9,19 +9,27 @@ namespace DalObject
 {
     public struct DataSource
     {
-        internal struct Config
+        /*internal struct Config
         {
             static internal int DronesIndex = 0;
             static internal int StationIndex = 0;
             static internal int CustomerIndex = 0;
             static internal int ParcialIndex = 0;
             static internal int DroneChargeIndex = 0;
-        }
-        internal static Drone[] Drones = new Drone[10];
+        }*/
+
+        internal static List<Drone> drones;
+        internal static List<Station> stations;
+        internal static List<Customer> customers;
+        internal static List<Parcial> parcials;
+        internal static List<DroneCharge> droneChargers;
+
+
+        /*internal static Drone[] Drones = new Drone[10];
         internal static Station[] Stations = new Station[5];
         internal static Customer[] Customers = new Customer[100];
         internal static Parcial[] Parcials = new Parcial[1000];
-        internal static DroneCharge[] DroneChargers = new DroneCharge[0];
+        internal static DroneCharge[] DroneChargers = new DroneCharge[0];*/
 
         public static void Initialize()
         {
@@ -29,50 +37,54 @@ namespace DalObject
             int r = rand.Next(2,5);
             for (int i = 0; i < r; i++)
             {
-                Stations[Config.StationIndex].ID = Config.StationIndex + 1;
-                Stations[Config.StationIndex].Name = Config.StationIndex + 1;
-                Stations[Config.StationIndex].Latitude = rand.Next();
-                Stations[Config.StationIndex].Longitude = rand.Next();
-                Stations[Config.StationIndex].ChargeSlots = rand.Next(300);
-                Config.StationIndex++;
+                Station station = new Station();
+                station.ID = stations.Count() + 1;
+                station.Name = stations.Count() + 1;
+                station.Latitude = rand.Next();
+                station.Longitude = rand.Next();
+                station.ChargeSlots = rand.Next(300);
+                stations.Add(station);
             }
 
             r = rand.Next(5, 10);
             for (int i = 0; i < r; i++)
             {
-                Drones[Config.DronesIndex].ID = Config.DronesIndex + 1;
-                Drones[Config.DronesIndex].Model = "MarvicAir2";
-                Drones[Config.DronesIndex].MaxWeight = WeightCatagories.Heavy + i;
-                Drones[Config.DronesIndex].Status = DroneStatus.Delivery + i;
-                Drones[Config.DronesIndex].Battery = rand.Next(100);
-                Config.DronesIndex++;
+                Drone drone = new Drone();
+                drone.ID = drones.Count() + 1;
+                drone.Model = "MarvicAir2";
+                drone.MaxWeight = WeightCatagories.Heavy + i;
+                drone.Status = DroneStatus.Delivery + i;
+                drone.Battery = rand.Next(100);
+                drones.Add(drone);
             }
 
             r = rand.Next(10, 15);
             for (int i = 0; i < r; i++)
             {
-                Customers[Config.CustomerIndex].ID = Config.CustomerIndex + 1;
-                Customers[Config.CustomerIndex].Name = $"customer{i}";
-                Customers[Config.CustomerIndex].Phone = $"{rand.Next(111111111, 999999999)}";
-                Customers[Config.CustomerIndex].Latitude = rand.Next();
-                Customers[Config.CustomerIndex].Longitude = rand.Next();
-                Config.CustomerIndex++;
+                Customer customer = new Customer();
+                customer.ID = customers.Count() + 1;
+                customer.Name = $"customer{i}";
+                customer.Phone = $"{rand.Next(111111111, 999999999)}";
+                customer.Latitude = rand.Next();
+                customer.Longitude = rand.Next();
+                customers.Add(customer);
             }
 
             r = rand.Next(10, 15);
             for (int i = 0; i < r; i++)
             {
-                Parcials[Config.ParcialIndex].ID = Config.ParcialIndex + 1;
-                Parcials[Config.ParcialIndex].SenderID = rand.Next() % Config.CustomerIndex;
-                Parcials[Config.ParcialIndex].TargetID = rand.Next() % Config.StationIndex;
-                Parcials[Config.ParcialIndex].Weight = (WeightCatagories)(rand.Next() %3);
-                Parcials[Config.ParcialIndex].Priority = (Priorities)(rand.Next() % 3);
-                Parcials[Config.ParcialIndex].Requested = new DateTime();
-                Parcials[Config.ParcialIndex].DroneID = rand.Next() % Config.DronesIndex;
-                Parcials[Config.ParcialIndex].Scheduled = new DateTime();
-                Parcials[Config.ParcialIndex].PickedUp = new DateTime();
-                Parcials[Config.ParcialIndex].Delivered = new DateTime(); 
-                Config.ParcialIndex++;
+                Parcial parcel = new Parcial();
+                parcel.ID = parcials.Count() + 1;
+                parcel.SenderID = rand.Next() % parcials.Count();
+                parcel.TargetID = rand.Next() % parcials.Count();
+                parcel.Weight = (WeightCatagories)(rand.Next() %3);
+                parcel.Priority = (Priorities)(rand.Next() % 3);
+                parcel.Requested = new DateTime();
+                parcel.DroneID = rand.Next() % parcials.Count();
+                parcel.Scheduled = new DateTime();
+                parcel.PickedUp = new DateTime();
+                parcel.Delivered = new DateTime();
+                parcials.Add(parcel);
 
             }
 
