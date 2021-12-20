10aab7127b442305095291cb4d99d7edeb21ad32
chayaBreisch <Handesaim3A5@mbjcomp.org.il>
2021-12-21 01:24:54 +0200

    change the binding in buttons enable in drone.xaml

---
+++ סמסטר א/C#/dotNet5782_9080_0477/PL1/ExceptionPL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL1
+{
+
+    public class InValidInput : Exception
+    {
+        public InValidInput(string input) : base($"invalid {input} input")
+        {
+
+        }
+    }
+}
