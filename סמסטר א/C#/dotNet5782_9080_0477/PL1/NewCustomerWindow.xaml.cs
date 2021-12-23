1c40af9a2347d0e549f44b04f27df7e83a276484
zipi <zipinewhous@gmail.com>
2021-12-23 08:30:50 +0200

    start the customer window

---
+++ סמסטר א/C#/dotNet5782_9080_0477/PL1/NewCustomerWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL1
+{
+    /// <summary>
+    /// Interaction logic for NewCustomerWindow.xaml
+    /// </summary>
+    public partial class NewCustomerWindow : Window
+    {
+        public NewCustomerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ClickJoin(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ulong id = (ulong)Convert.ToInt32(newID.Text);
+        }                               
+            }
+
+    }
+}
