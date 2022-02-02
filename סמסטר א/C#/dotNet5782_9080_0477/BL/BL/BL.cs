da7601fb604387f21c5be35cc2060f5a9f75d06a
chayaBreisch <handesaim3a5@mbjcomp.org.il>
2022-02-02 12:09:02 +0200

    FIX

--- סמסטר א/C#/dotNet5782_9080_0477/BL/BL/BL.cs
+++ סמסטר א/C#/dotNet5782_9080_0477/BL/BL/BL.cs
@@ -52,11 +52,11 @@ namespace BL
             {
                 BO.Drone droneBL = new BO.Drone { ID = drone.ID, Model = drone.Model, Weight = drone.MaxWeight, IsActive = drone.IsActive };
                 DO.Parcel parcel = dalObject.GetParcelBy(p => p.DroneID == drone.ID);
-                BO.Customer customerSender = GetSpecificCustomerBL(c => c.ID == parcel.SenderID);
                 //check if the drone has a parcel
-                if (parcel.SenderID != 10000000)
+                if (parcel.SenderID != 10000000 && parcel.SenderID != 0)
                 //if (!parcel.Equals(null))
                 {
+                    BO.Customer customerSender = GetSpecificCustomerBL(c => c.ID == parcel.SenderID);
                     //check if the parcel of the drone is scheduled and not delivered
                     if (parcel.Scheduled != null && parcel.Delivered == null)
                     {
